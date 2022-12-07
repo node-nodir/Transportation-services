86101bf5dcfa79615d14d55ea0446c81d44d25aa
kholiqulov_nodirbek <99204049+Xoliqulov-Nodirbek@users.noreply.github.com>
2022-12-07 13:20:31 +0500

    Add fix bug

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,10 +1,8 @@
 import React from "react";
+import Hero from '../Hero/Hero'
+import Wedo from "../Wedo/Wedo";
 import AboutUs from "../AboutUs/AboutUs";
 import OurServices from "../OurServices/OurServices";
-import Wedo from "../Wedo/Wedo";
-import React from 'react'
-import Hero from '../Hero/Hero'
-import OurServices from '../OurServices/OurServices'
 
 function Main() {
   return (
