8b68a4a825dfae980303f24b48aeba13b59db214
Nurillo18 <suyunboyevnurillo18@gmail.com>
2022-12-07 13:34:06 +0500

    fix all

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import Hero from '../Hero/Hero'
+import Hero from "../Hero/Hero";
 import Wedo from "../Wedo/Wedo";
 import AboutUs from "../AboutUs/AboutUs";
 import OurServices from "../OurServices/OurServices";
-import FormikContainer from '../BaseComponents/FormInput/FormikContainer'
 
 function Main() {
   return (
     <main>
-      <FormikContainer />
       <Hero />
       <OurServices />
       <Wedo />
