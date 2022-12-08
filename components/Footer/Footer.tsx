e5adb45d07852e3bc5ff4a214dc1f2f201f5e7d9
IlkhamBurkhonov <ilkhomjonburkhonov207@gmail.com>
2022-12-08 13:25:08 +0500

    footer almosst Ready

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -10,21 +10,21 @@ function Footer(): any {
   
     <footer className="w-full  bg-[#282828]">
       <div className="container">
-        <div className="md:flex md:justify-between gap-2 sm:grid-cols-2 sm:justify-between grid grid-cols-1 md:max-h-[250px]">
-          <div className="max-w-354 w-full flex-shrink-0 mt-12 md:my-[50px] ">
-           <Link href="#"> <Image src={Main} width={245} height={43} alt="logo" /></Link>
-            <p className="font-light text-base leading-[19px] lg:mt-3 lg:mb-8 text-[#ffffffde]">
+        <div className="md:flex md:justify-between gap-2 sm:grid-cols-2 sm:justify-between grid grid-cols-1 justify-center text-center md:text-start md:max-h-[250px]">
+          <div className="md:max-w-354 w-full flex-shrink-0 mt-12 md:my-[50px] justify-center">
+           <Link href="#"> <Image src={Main} alt="logo" className="md:w-[245px] mx-auto  mb-9  md:h-[43px] w-[202px] h-10"/></Link>
+            <p className="hidden md:block font-light text-base leading-[19px] lg:mt-3 lg:mb-8 text-[#ffffffde]">
               Transportation Services that you can trust
             </p>
-            <h1 className="font-light text-[#ffffff61] mt-[55px]  text-xs my-8 text-start">
+            <h1 className="hidden md:block font-light text-[#ffffff61] mt-[55px]  text-xs my-8 text-start">
               Copyright © All rights reserved
             </h1>
           </div>
-          <div className="max-w-155 max-h-197 w-full my-5 md:mt-[50px]">
+          <div className="md:max-w-155 max-h-197 w-full my-4 md:mt-[50px]">
             <h1 className="text-lg font-semibold leading-7 text-[#ffffffde]">
               Our Services
             </h1>
-            <ul className="text-[#ffffffad] mt-[20px]">
+            <ul className="text-[#ffffffad] mt-4  md:mt-5">
               <li className="text-sm hover:underline underline-offset-2">
                 <a href="#">Car Shipments</a>
               </li>
@@ -39,11 +39,11 @@ function Footer(): any {
               </li>
             </ul>
           </div>
-          <div className="max-w-166 max-h-131 w-full my-5 md:mt-[50px]">
+          <div className="md:max-w-166 max-h-131 w-full my-4 md:mt-[50px]">
             <h1 className="text-lg font-semibold leading-7 text-[#ffffffde]">
               Shipping Methods
             </h1>
-            <ul className="text-[#ffffffad] mt-[20px]">
+            <ul className="text-[#ffffffad] mt-4  md:mt-5">
               <li className="text-sm hover:underline underline-offset-2">
                 <a href="#">Rail Freight</a>
               </li>
@@ -55,11 +55,11 @@ function Footer(): any {
               </li>
             </ul>
           </div>
-          <div className="max-w-155 max-h-197 w-full  my-5 md:mt-[50px]">
+          <div className="md:max-w-155 max-h-197 w-full  my-4 md:mt-[50px]">
             <h1 className="text-lg font-semibold leading-7 text-[#ffffffde]">
               About US
             </h1>
-            <ul className="text-[#ffffffad] mt-[20px]">
+            <ul className="text-[#ffffffad] mt-4  md:mt-5">
               <li className="text-sm hover:underline underline-offset-2">
                 <a href="#">Our Company</a>
               </li>
@@ -69,11 +69,11 @@ function Footer(): any {
               <li className="mt-3 text-sm hover:underline underline-offset-2">
                 <a href="#">Contact us</a>
               </li>
-              <li className="mt-3 text-sm hover:underline underline-offset-2">
-                <a href="#">Terms of Service</a>
-              </li>
             </ul>
           </div>
+            <h1 className="md:hidden font-light text-[#ffffff61] mt-16 mb-12 text-xs my-8 text-center">
+                  Copyright © All rights reserved
+            </h1>
         </div>
       </div>
     </footer>
