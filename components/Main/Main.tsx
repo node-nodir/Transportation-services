33bbec5d559f7bc2fd0dc91232e1e7e55bf2d370
IlkhamBurkhonov <ilkhomjonburkhonov207@gmail.com>
2022-12-08 08:33:44 +0500

    no change

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,11 +1,11 @@
 
 import React from 'react'
 import OurServices from '../OurServices/OurServices'
-import React from "react";
+
 import Hero from "../Hero/Hero";
 import Wedo from "../Wedo/Wedo";
 import AboutUs from "../AboutUs/AboutUs";
-import OurServices from "../OurServices/OurServices";
+
 import Shipping from "../Shipping/Shipping";
 import Frequently from "../Frequently/Frequently";
 
